d9a0e6f8810f32e46c5c76b350e3a78735e4ed1a
Samir <125750491+sam-k9@users.noreply.github.com>
2023-07-21 15:42:45 +0530

    First Commit

---
+++ src/components/SearchForm.jsx
@@ -0,0 +1,25 @@
+import Wrapper from "../assets/wrappers/SearchForm";
+import { Form, useNavigate, useNavigation } from "react-router-dom";
+
+const SearchForm = ({ searchTerm }) => {
+  const navigation = useNavigation();
+  const isSubmitting = navigation.state === "submitting";
+
+  return (
+    <Wrapper>
+      <Form className="form">
+        <input
+          type="search"
+          name="search"
+          className="form-input"
+          defaultValue={searchTerm}
+        />
+        <button type="submit" className="btn" disabled={isSubmitting}>
+          {isSubmitting ? "Searching..." : "Search"}
+        </button>
+      </Form>
+    </Wrapper>
+  );
+};
+
+export default SearchForm;
---
+++ src/main.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "react-toastify/dist/ReactToastify.css";
+import "./index.css";
+import { ToastContainer } from "react-toastify";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <ToastContainer position="top-center" autoClose={2000} />
+    <App />
+  </React.StrictMode>
+);
